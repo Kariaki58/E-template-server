b45e5e8a8f4f32cb8f46292a06db474145275dc6
Kariaki58 <kariakistephen809@gmail.com>
2024-08-16 16:07:14 +0100

    worked on getting user cart

--- controllers/buyers/cart/carts.mjs
+++ controllers/buyers/cart/carts.mjs
@@ -1,3 +1,17 @@
-export const getUserCart = (req, res) => {
-    // get user cart
+import Cart from "../../../models/carts.mjs"
+
+export const getUserCart = async (req, res) => {
+    try {
+        const user = req.user
+
+        const findUserCart = await Cart.findOne({ userId: user })
+
+        if (!findUserCart) {
+            return res.status(200).send({ message: [] })
+        }
+        return res.status(200).send({ message: findUserCart.items })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({ error: "server error" })
+    }
 }
