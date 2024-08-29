947c12d837fa4d13e4ed01ce73249f71509f036e
Kariaki58 <kariakistephen809@gmail.com>
2024-08-29 19:36:59 +0100

    removed frontend env path

--- app.mjs
+++ app.mjs
@@ -12,13 +12,21 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: process.env.FRONTEND,
+    origin: "https://kariaki-template-beta-1.netlify.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }
 
 
 app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://kariaki-template-beta-1.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+  
 app.use(ExpressMongoSanitize())
 app.use(cookieParser())
 app.use(route)
