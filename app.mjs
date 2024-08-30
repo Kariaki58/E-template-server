07f909246a018f6c83f75ca8bdc48a3fa5e0161a
Kariaki58 <kariakistephen809@gmail.com>
2024-08-30 04:15:24 +0100

    trying to deploy changes

--- app.mjs
+++ app.mjs
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: "https://kariaki-stephens-projects-0b9759d8.vercel.app/",
+    origin: process.env.FRONTEND || "https://kariaki.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://kariaki-stephens-projects-0b9759d8.vercel.app/");
+    res.header("Access-Control-Allow-Origin", process.env.FRONTEND || "https://kariaki.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
