1bc62cc9591b6c82cbd0c0e4d7cebc1ac4fbfd0d
Kariaki58 <kariakistephen809@gmail.com>
2024-08-30 04:02:40 +0100

    using vercels frontend

--- app.mjs
+++ app.mjs
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: "https://kariaki-template-beta-1.netlify.app",
+    origin: "https://kariaki-stephens-projects-0b9759d8.vercel.app/",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }
@@ -21,7 +21,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://kariaki-template-beta-1.netlify.app");
+    res.header("Access-Control-Allow-Origin", "https://kariaki-stephens-projects-0b9759d8.vercel.app/");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
--- middleware/auth.mjs
+++ middleware/auth.mjs
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 
 export const generateToken = (_id) => {
-    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '4h' });
+    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
 export const authenticateToken = (req, res, next) => {
