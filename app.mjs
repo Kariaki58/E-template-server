cb1c25d9607902f5630d94894760de08e6b7572f
Kariaki58 <kariakistephen809@gmail.com>
2024-09-08 12:41:42 +0100

    changed frontend path

--- app.mjs
+++ app.mjs
@@ -14,14 +14,14 @@ dotenv.config();
 const app = express();
 
 // Middleware setup
-app.use(express.json({ limit: '50kb' }));  // Limit request body size to prevent attacks
+app.use(express.json({ limit: '100kb' }));  // Limit request body size to prevent attacks
 app.use(ExpressMongoSanitize());
 app.use(cookieParser());
 app.use(compression());  // Compress responses
 app.use(helmet());  // Set security-related HTTP response headers
 
 // CORS configuration
-const allowedOrigins = 'http://localhost:5173'
+const allowedOrigins = 'https://apiduct.vercel.app'
 const corsOptions = {
   origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
